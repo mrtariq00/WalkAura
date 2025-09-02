aea379f22509de5d3b05d74b33a31d7e33f03537
Mr_TARIQ <88210527+mrtariq00@users.noreply.github.com>
2025-09-02 19:56:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function release() {
     newlist.innerHTML += `
 <div class="col-6 col-md-3">
     <div class="product-card text-center">
-    <a href="WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
+    <a href="/WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
         <img src="${p.img}" alt="${p.name}">
         <h6 class="mt-2 fw-bold">${p.name}</h6>
         <p>$${p.price} USD</p>
@@ -33,7 +33,7 @@ function topPriceShoes() {
     toplist.innerHTML += `
 <div class="col-6 col-md-3">
     <div class="product-card text-center">
-    <a href="WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
+    <a href="/WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
         <img src="${p.img}" alt="${p.name}">
         <h6 class="mt-2 fw-bold">${p.name}</h6>
         <p>$${p.price} USD</p>
@@ -49,4 +49,5 @@ topPriceShoes();
 {
   /* <a href="/petbot/pages/product.html?name=${p.name}&img=${p.img}&color=${p.color}&qty=${p.qty}&description=${p.description}&price=${p.price}" class="text-decoration-none"></a> */
 }
+
 
