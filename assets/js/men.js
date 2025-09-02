03ff5188490bda2e706fb828ef97965582b5dbc9
Mr_TARIQ <88210527+mrtariq00@users.noreply.github.com>
2025-09-02 19:55:38 +0500

    Update men.js

--- assets/js/men.js
+++ assets/js/men.js
@@ -6,7 +6,7 @@ function menlist() {
         manlist.innerHTML += `
     <div class="col-6 col-md-3">
     <div class="product-card text-center">
-    <a href="WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
+    <a href="/WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
         <img src="${p.img}" alt="${p.name}">
         <h6 class="mt-2 fw-bold">${p.name}</h6>
         <p>$${p.price} USD</p>
@@ -18,3 +18,4 @@ function menlist() {
 
 
 menlist()
+
