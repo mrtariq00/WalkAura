d752cde9e7e1c5aa12ed6491040ca96faf76f98c
Mr_TARIQ <88210527+mrtariq00@users.noreply.github.com>
2025-09-02 19:46:31 +0500

    Update kid.js

--- assets/js/kid.js
+++ assets/js/kid.js
@@ -6,7 +6,7 @@ function kidslist() {
         kidlist.innerHTML += `
     <div class="col-6 col-md-3">
     <div class="product-card text-center">
-    <a href="/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
+    <a href="WalkAura/assets/pages/product.html?name=${p.name}&img=${p.img}&gender=${p.gender}&rater=${p.raters}&description=${p.description}&price=${p.price}&rating=${p.rating}" class="text-decoration-none">
         <img src="${p.img}" alt="${p.name}">
         <h6 class="mt-2 fw-bold">${p.name}</h6>
         <p>$${p.price} USD</p>
@@ -15,5 +15,6 @@ function kidslist() {
 </div>`;
     });
 }
+
 
 kidslist()
